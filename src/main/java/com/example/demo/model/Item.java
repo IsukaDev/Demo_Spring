6a40a414f143a8eeff9b1695d136480020268ffe
Anne Géraud <52383742+Kallypige@users.noreply.github.com>
2022-05-18 14:54:49 +0200

    Implémentation d'id auto-généré

--- src/main/java/com/example/demo/model/Item.java
+++ src/main/java/com/example/demo/model/Item.java
@@ -1,6 +1,8 @@
 package com.example.demo.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -9,6 +11,7 @@ public class Item {
 	private String name;
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Item(String name, Long id) {
---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into item (name)
+values ('book');
+insert into item (name)
+values ('shelf');
+insert into item (name)
+values ('computer');
+
